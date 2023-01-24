646bd6a23a669dd0092eafb4cd9457f5d716b3f0
anafmonteiro <ana.monteiro2014@engenharia.ufjf.br>
2023-01-24 15:30:07 -0300

    include all informations on a api file and create a layergroup component

--- src/Components/Zone/DemandZone/index.tsx
+++ src/Components/Zone/DemandZone/index.tsx
@@ -12,32 +12,34 @@ interface ZoneProps {
     information:any;
     pathOptions: { color: string, fillColor: string };
     radius?:number;
+    onClick: (value:string) => void;
 }
 
 const DemandZone:React.FC<ZoneProps> = (props:ZoneProps) => {
     const {information} = props
     return (
-    <Circle 
-        key={props.index}
-        center={information.geometry.demand_coordinates} 
-        pathOptions={props.pathOptions} 
-        radius={props.radius || 200000} 
-    >
-         <Tooltip direction="top" offset={[10, 0]}>
-            <TooltipContainer>
-                <Label>Cidade de destino:</Label>
-                <InformationText>{information.geometry.demand_name}</InformationText>
-            </TooltipContainer>
-            <TooltipContainer>
-                <Label>Cidade de origem:</Label>
-                <InformationText>{information.geometry.origin_name}</InformationText>
-            </TooltipContainer>
-            <TooltipContainer>
-                <Label>Volume de demanda:</Label>
-                <InformationText>{information.geometry.radius}</InformationText>
-            </TooltipContainer>
-        </Tooltip>
-    </Circle>
+        <Circle 
+            key={props.index}
+            center={information.geometry.demand_coordinates} 
+            pathOptions={props.pathOptions} 
+            radius={props.radius || 200000} 
+            eventHandlers={{ click: ()=> props.onClick(information)}}
+        >
+            <Tooltip direction="top" offset={[10, 0]} >
+                <TooltipContainer>
+                    <Label>Cidade de destino:</Label>
+                    <InformationText>{information.geometry.demand_name}</InformationText>
+                </TooltipContainer>
+                <TooltipContainer>
+                    <Label>Cidade de origem:</Label>
+                    <InformationText>{information.geometry.origin_name}</InformationText>
+                </TooltipContainer>
+                <TooltipContainer>
+                    <Label>Volume de demanda:</Label>
+                    <InformationText>{information.geometry.radius}</InformationText>
+                </TooltipContainer>
+            </Tooltip>
+        </Circle>
     )
 }
 
